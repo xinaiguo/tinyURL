c140009b1ca6549f7c96365712b1e68081a7378b
Xinai <xinai.guo@siemens.com>
2019-11-14 14:16:44 +0800

    tinyurl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,59 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  longURL = 'https://www.amazon.com/this-is-a-complex-path/design-tinyurl';
-  tinyURL:string;
+  longURL: string;
+  tinyURL: string;
+  originURL: string;
+  map = new Map();
 
-  createTinyURL(longURL:string) {
-    let map = [];
-    let i = 0;
-    map[i] = longURL;
-    this.tinyURL = `https://www.amazon.com/${i++}`;
-    console.log(map);
+  ngOnInit() {
+    this.longURL = '';
   }
+
+  createTinyURL(longURL: string) {
+    let i = this.hashCode(longURL);
+    let code = longURL.split('/')[0] + longURL.split('/')[1] + '//' + longURL.split('/')[2] + '/';
+    this.map.set(this.base10ToBase62(Math.abs(i)), longURL);
+    this.tinyURL = code + this.base10ToBase62(Math.abs(i));
+    console.log(this.map);
+  }
+
+  getOriginURL(shortUrl) {
+    let host = shortUrl.split('/')[0] + shortUrl.split('/')[1] + '//' + shortUrl.split('/')[2] + '/'
+    let code = shortUrl.replace(host, "");
+    this.originURL = this.map.get(code);
+  }
+
+  //convert longurl to hashcode
+  hashCode(str) {
+    let hash = 0, i, chr, len;
+    if (str.length === 0) return hash;
+    for (i = 0, len = str.length; i < len; i++) {
+      chr = str.charCodeAt(i);
+      hash = ((hash << 6) - hash) + chr;
+      hash |= 0;
+    }
+    return hash;
+  }
+
+
+  base10ToBase62(number) {
+    let chars = '0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ'.split(''),
+      radix = chars.length,
+      qutient = +number,
+      arr = [];
+    do {
+      let mod = qutient % radix;
+      qutient = (qutient - mod) / radix;
+      arr.unshift(chars[mod]);
+    } while (qutient);
+    return arr.join('');
+  }
+
 }
